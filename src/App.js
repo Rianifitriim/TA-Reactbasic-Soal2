2d2ea43e2119ae2852f5347259b06bd3d7eb725e
Rianifitriim <rianifitri53@gmail.com>
2021-10-24 21:44:44 +0700

    reactbasic soal 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import MovieCard from "./MovieCard";
+// import animeList from "./dummy-data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieList: [],
+      isLoading: true
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.fetchMovieList(), 1000);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.movieList.length !== this.state.movieList.length) {
+      this.setState({
+        isLoading: false
+      });
+    }
+  }
+
+  async fetchMovieList() {
+    const response = await fetch("https://api.jikan.moe/v3/top/anime");
+    const result = await response.json();
+    this.setState({
+      movieList: result.top || []
+    });
+  }
+
+  render() {
+    return (
+      <div className="container my-5">
+        {/* <h1 className="text-center">{this.state.isLoading ? "sedang loading" : "Sudah tidak loading"}</h1> */}
+        <h1 className="text-center">Anime List</h1>
+        <div className="container my-5">
+          <div id="daftar-anime" className="row">
+            {this.state.isLoading ? (
+              <h1>Loading...</h1>
+            ) : (
+              this.state.movieList.map((movie) => (
+                <MovieCard key={movie.mal_id} movie={movie} />
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
